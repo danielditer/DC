6d9c568add49e73723bf45c4c811f98f9213e586
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-07-31 18:57:26 -0400

    Added task 4

--- gradle/task3/src/main/java/org/gradle/Person.java
+++ gradle/task3/src/main/java/org/gradle/Person.java
@@ -7,14 +7,27 @@ import org.apache.commons.collections.list.GrowthList;
  */
 public class Person {
     private final String name;
+    private final String lastName;
+    private int age;
+    private Gender gender;
+    private PersonalInformation personalInformation;
 
     /**
      * Class constructor.
      *
-     * @param name the name of the person.
+     * @param name                the name of the person.
+     * @param lastName            the last name.
+     * @param age                 the age.
+     * @param gender              the gender.
+     * @param personalInformation the current data of the person.
      */
-    public Person(String name) {
+
+    public Person(String name, String lastName, int age, Gender gender, PersonalInformation personalInformation) {
         this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.personalInformation = personalInformation;
         new GrowthList();
     }
 
@@ -26,4 +39,60 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    /**
+     * Getter for last name.
+     *
+     * @return the last name of the person.
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Getter for age.
+     *
+     * @return the age of the person.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Getter for gender.
+     *
+     * @return the gender of the person.
+     */
+    public Gender getGender() {
+        return gender;
+    }
+
+
+    /**
+     * Getter for full name.
+     *
+     * @return the full name of the person.
+     */
+    public String getFullName() {
+        return String.format("%s %s",  getName(),  getLastName());
+    }
+
+    /**
+     * Getter for signature.
+     *
+     * @return the person signature.
+     */
+    public String getSignature() {
+        return String.format("%s CI: %s",  getFullName(),  getPersonalInformation().getCi());
+    }
+
+    /**
+     * Getter for personal information.
+     *
+     * @return the personal information object.
+     */
+    private PersonalInformation getPersonalInformation() {
+        return personalInformation;
+    }
+
 }
--- gradle/task3/src/test/java/org/gradle/PersonTest.java
+++ gradle/task3/src/test/java/org/gradle/PersonTest.java
@@ -1,18 +1,130 @@
 package org.gradle;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Class for testing 'Person' class.
  */
 public class PersonTest {
+
+    private Person person;
+    private PersonalInformation personalInformation;
+
+    /**
+     * Initializes variables.
+     */
+    @Before
+    public void setUp() {
+        final int ci = 6543210;
+        final int phoneNumber = 77665544;
+        personalInformation = new PersonalInformation(CivilStatus.MARRIED, "Av. Simon Lopez #2643",
+                ci, phoneNumber, "Automation Engineer");
+        final int age = 50;
+        person = new Person("Larry", "Page", age, Gender.MALE, personalInformation);
+    }
+
     /**
      * Method for testing 'getName()' method.
      */
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
+
         assertEquals("Larry", person.getName());
+    }
+
+    /**
+     * Method for testing 'getLastName()' method.
+     */
+    @Test
+    public void canConstructAPersonWithALastName() {
+
+        assertEquals("Page", person.getLastName());
+    }
+
+    /**
+     * Method for testing 'getAge()' method.
+     */
+    @Test
+    public void canConstructAPersonWithAge() {
+
+        final int expected = 50;
+        assertEquals(expected, person.getAge());
+    }
+
+    /**
+     * Method for testing 'getGender()' method.
+     */
+    @Test
+    public void canConstructAPersonWithGender() {
+
+        assertEquals(Gender.MALE, person.getGender());
+    }
+
+    /**
+     * Method for testing 'getCivilStatus()' method.
+     */
+    @Test
+    public void canConstructAPersonWithCivilStatus() {
+
+        assertEquals(CivilStatus.MARRIED, personalInformation.getCivilStatus());
+    }
+
+    /**
+     * Method for testing 'getAddress()' method.
+     */
+    @Test
+    public void canConstructAPersonWithAddress() {
+
+        assertEquals("Av. Simon Lopez #2643", personalInformation.getAddress());
+    }
+
+    /**
+     * Method for testing 'getCi()' method.
+     */
+    @Test
+    public void canConstructAPersonWithCI() {
+
+        final int expected = 6543210;
+        assertEquals(expected, personalInformation.getCi());
+    }
+
+    /**
+     * Method for testing 'getCi()' method.
+     */
+    @Test
+    public void canConstructAPersonWithPhoneNumber() {
+
+        final int expected = 77665544;
+        assertEquals(expected, personalInformation.getPhoneNumber());
+    }
+
+    /**
+     * Method for testing 'getCi()' method.
+     */
+    @Test
+    public void canConstructAPersonWithOccupation() {
+
+        assertEquals("Automation Engineer", personalInformation.getOccupation());
+    }
+
+    /**
+     * Method for testing 'getFullName()' method.
+     */
+    @Test
+    public void canGetAPersonFullName() {
+
+        assertEquals("Larry Page", person.getFullName());
+    }
+
+    /**
+     * Method for testing 'getSignature()' method.
+     */
+    @Test
+    public void canGetAPersonSignature() {
+
+        assertEquals("Larry Page CI: 6543210", person.getSignature());
     }
 }
